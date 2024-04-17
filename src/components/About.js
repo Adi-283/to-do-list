62e40b4b356302a7ccd0c1aba37329f9a1afe93f
Aditya Mehta <adimehta283@gmail.com>
2024-04-17 12:24:56 -0400

    created about us page

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+function About() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is the final project for the Trends in Web Development course of Level 4.</p>
+        <p>We are Group 10 of Section 4.</p>
+        <p>Our group comprises of 4 members</p>
+        <ol>
+            <li>Patel Meet Vimalkumar – 8882879 </li>
+            <li>Patel Pratikkumar Harshadbhai – 8868235 </li>
+            <li>Patel Prince Janakbhai – 8867414 </li>
+            <li>Mehta Aditya Mehul - 8866740 </li>
+        </ol>
+      </div>
+    );
+  }
+  
+  export default About;
