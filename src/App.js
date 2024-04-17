8bd54bbe829e18883ce4b78e76889b5990eaf0db
Aditya Mehta <adimehta283@gmail.com>
2024-04-17 12:44:46 -0400

    added logic of the to-do list app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './/components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: Date.now(), text: newTodoText, completed: false }]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          {/* Your future components will go here */}
-        </header>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<Home addTodo={addTodo} todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo}  />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   );
