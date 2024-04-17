61d418c40e071ae2f33ee11afea9c019d35f8d19
Aditya Mehta <adimehta283@gmail.com>
2024-04-17 13:10:12 -0400

    added a footer the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import './index.css';
 import Navbar from './/components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -29,6 +31,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 To-Do List. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
